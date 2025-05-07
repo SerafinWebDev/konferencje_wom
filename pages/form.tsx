b699bb57e2f2885017c1fda81fabda2201da313b
SerafinWebDev <wom.katowice.tech@gmail.com>
2025-05-07 15:58:38 +0200

    expose to client component with suspense

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,16 +1,17 @@
 'use server'
-
+import { Suspense } from 'react'
 
 import {CustomForm} from "@/components/form";
 
-export default async function CustomFormPage() {
+export default  function CustomFormPage() {
 
 
 
   return (
     <div className="h-screen w-full flex justify-center items-center bg-black">
-     
+      <Suspense fallback={<div>Loading...</div>}>
       <CustomForm />
+    </Suspense>
     </div>
   );
 }
