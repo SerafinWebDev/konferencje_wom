d2b60f8fe3a4a45d91e33c9223ca200bdb081998
SerafinWebDev <wom.katowice.tech@gmail.com>
2025-05-07 15:52:58 +0200

    expose to client component

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,32 +1,16 @@
 'use server'
 
-import {
-    getAllUsers,
-    UserProps,
-    getUserCount,
-    getFirstUser,addUser
-  } from '@/lib/api/user';
+
+import {CustomForm} from "@/components/form";
+
+export default async function CustomFormPage() {
 
 
-export default async function CustomForm() {
-
-
-const clickHandler = async () => await addUser('Tedo', 'Testowy', 'mail@testy.pl')
 
   return (
     <div className="h-screen w-full flex justify-center items-center bg-black">
      
-      <h1 className="text-2xl font-light text-white">
-       FORM
-      </h1>
-
-      <button
-        //   onClick={clickHandler }
-          className="rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          type="button"
-        >
-          Bubu
-        </button>
+      <CustomForm />
     </div>
   );
 }
